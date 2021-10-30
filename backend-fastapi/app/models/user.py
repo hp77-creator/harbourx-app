4c0b7df3e919fccad48c35bbd849f0801a993777
Himanshu Pandey <hp77codeforgood@gmail.com>
2021-10-30 18:29:13 +0530

    Add model for user
    Signed-off-by: Himanshu Pandey <hp77codeforgood@gmail.com>

--- backend-fastapi/app/models/user.py
+++ backend-fastapi/app/models/user.py
@@ -1,1 +1,13 @@
+from enum import unique
+from sqlalchemy import ForeignKey, Column, Integer, String
+
+from app.db.base_class import Base
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False, unique=True)
+    email = Column(String, unique=True, index=True)
+
+    
 
